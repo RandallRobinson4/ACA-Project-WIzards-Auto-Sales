4198fab7cd33beea4c7a624e2bb13ac3252a944b
Randall Robinson <keithadams4444@gmail.com>
2019-12-10 12:44:26 -0600

    make changes to object structure

--- wizardsautosales/src/main/java/com/aca/project/model/Car.java
+++ wizardsautosales/src/main/java/com/aca/project/model/Car.java
@@ -4,17 +4,19 @@ import java.math.BigDecimal;
 
 public class Car {
 
-		private Make model;
+		private Make make;
 		private Interior interior;
 		private String color;
 		private int mileage;
 		private BigDecimal price;
 		private int id;
 		private int avgMPG;
+		private Model model;
+		private int modelYear;
 		
 		public Car() {}
 		
-		public Car(Make model, Interior interior, String color, int mileage, BigDecimal price, int id, int avgMPG) {
+		public Car(Make make, Interior interior, String color, int mileage, BigDecimal price, int id, int avgMPG, Model model, int modelYear) {
 			this.model = model;
 			this.interior = interior;
 			this.color = color;
@@ -22,6 +24,25 @@ public class Car {
 			this.price = price;
 			this.id = id;
 			this.avgMPG = avgMPG;
+			this.make = make;
+			this.modelYear = modelYear;
+		}
+
+
+		public Model getModel() {
+			return model;
+		}
+
+		public int getModelYear() {
+			return modelYear;
+		}
+
+		public void setModelYear(int modelYear) {
+			this.modelYear = modelYear;
+		}
+
+		public void setModel(Model model) {
+			this.model = model;
 		}
 
 		public int getAvgMPG() {
@@ -32,12 +53,12 @@ public class Car {
 			this.avgMPG = avgMPG;
 		}
 
-		public Make getModel() {
-			return model;
+		public Make getMake() {
+			return make;
 		}
 
-		public void setModel(Make model) {
-			this.model = model;
+		public void setMake(Make make) {
+			this.make = make;
 		}
 
 		public Interior getInterior() {
--- wizardsautosales/src/main/java/com/aca/project/model/Make.java
+++ wizardsautosales/src/main/java/com/aca/project/model/Make.java
@@ -3,16 +3,12 @@ package com.aca.project.model;
 public class Make {
 	
 	private String name;
-	private Model model;
-	private int modelYear;
 	private int id;
 	
 	public Make() {}
 	
-	public Make(String name, Model model, int modelYear, int id) {
+	public Make(String name,  int id) {
 		this.name = name;
-		this.model = model;
-		this.modelYear = modelYear;
 		this.id = id;
 	}
 
@@ -31,22 +27,4 @@ public class Make {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public Model getModel() {
-		return model;
-	}
-
-	public void setMake(Model model) {
-		this.model = model;
-	}
-
-	public int getModelYear() {
-		return modelYear;
-	}
-
-	public void setModelYear(int modelYear) {
-		this.modelYear = modelYear;
-	}
-	
-
 }
