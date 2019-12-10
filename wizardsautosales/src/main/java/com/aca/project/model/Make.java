eaa55fd2c3b68a416141da76f5d4308761d51350
Randall Robinson <keithadams4444@gmail.com>
2019-12-09 20:08:06 -0600

    added Make class

---
+++ wizardsautosales/src/main/java/com/aca/project/model/Make.java
@@ -0,0 +1,30 @@
+package com.aca.project.model;
+
+public class Make {
+	
+	private String name;
+	private int id;
+	
+	public Make() {}
+	
+	public Make(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
