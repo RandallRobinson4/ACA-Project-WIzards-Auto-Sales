d6b760b8136a7c5deb947b3900cb1f98c09e4701
Randall Robinson <keithadams4444@gmail.com>
2019-12-18 10:09:42 -0600

    working the html and css before i start js

--- wizardsautosales/src/main/webapp/js/car.js
+++ wizardsautosales/src/main/webapp/js/car.js
@@ -1,1 +1,35 @@
 let carApp = angular.module("carApp", ["ngRoute"]);
+
+
+carApp.config(function($routeProvider) {
+	$routeProvider
+	.when("/inventory", {
+		templateUrl : "inventory.html"
+	})
+	.when("/tour", {
+		templateUrl : "tour.html"
+	})
+	.when("/stack", {
+		templateUrl : "stack.html"
+	})
+	.when("/resume", {
+		templateUrl : "resume.html"
+	})
+	.when("/admin", {
+		templateUrl : "admin.html"
+	})
+	.when("/hobbies", {
+		templateUrl : "hobbies.html"
+	})
+	.when("/apprenticeClub", {
+		templateUrl : "apprenticeClub.html",
+		controller : "apprenticeClubController"
+	})
+	.otherwise({
+		templateUrl : "welcome.html"
+	});
+});
+
+carApp.controller("carController", function($scope, $http) {
+	
+});
